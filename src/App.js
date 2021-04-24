e7ff710e9335f7a8cf7f2be4fac3ab72d538aafc
James <conwayjw97@gmail.com>
2021-04-24 12:54:29 +0100

    Moved settings state logic to parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Canvas from './components/canvas/Canvas.js';
 import SettingsMenu from './components/settings_menu/SettingsMenu.js';
 import './App.css';
 
+import {
+  degToRad,
+  radToDeg
+} from "./utils/angleCalc.js";
+
 function App() {
+  const [degreeAngle, setDegreeAngle] = useState(0);
+  const [radianAngle, setRadianAngle] = useState(0);
+
+  const handleDegreeAngleChange = (event) => {
+    const input = event.target.value;
+    if(parseInt(input) >= 0 && parseInt(input) <= 360){
+      setDegreeAngle(input);
+      setRadianAngle(degToRad(input).toFixed(5));
+    }
+    else{
+      console.log("ANGLE OUT OF BOUNDS");
+    }
+  }
+
+  const handleRadianAngleChange = (event) => {
+    const input = event.target.value;
+    setRadianAngle(input);
+    setDegreeAngle(radToDeg(input).toFixed(0));
+  }
+
   return (
     <div className="App">
-      <SettingsMenu />
+      <SettingsMenu degreeAngle={degreeAngle} radianAngle={radianAngle} handleDegreeAngleChange={handleDegreeAngleChange} handleRadianAngleChange={handleRadianAngleChange}/>
       <Canvas />
     </div>
   );
--- src/components/settings_menu/SettingsMenu.js
+++ src/components/settings_menu/SettingsMenu.js
@@ -2,39 +2,14 @@ import React, { useState, useRef } from "react";
 import { slide as Menu } from 'react-burger-menu';
 import "./SettingsMenu.css";
 
-import {
-  degToRad,
-  radToDeg
-} from "../../utils/angleCalc.js";
-
-function SettingsMenu() {
-  const [degreeAngle, setDegreeAngle] = useState(0);
-  const [radianAngle, setRadianAngle] = useState(0);
-
-  const handleDegreeAngleChange = (event) => {
-    const input = event.target.value;
-    if(parseInt(input) >= 0 && parseInt(input) <= 360){
-      setDegreeAngle(input);
-      setRadianAngle(degToRad(input).toFixed(5));
-    }
-    else{
-      console.log("ANGLE OUT OF BOUNDS");
-    }
-  }
-
-  const handleRadianAngleChange = (event) => {
-    const input = event.target.value;
-    setRadianAngle(input);
-    setDegreeAngle(radToDeg(input).toFixed(0));
-  }
-
+function SettingsMenu(props) {
   return (
     <Menu className="settings-menu" left noOverlay disableAutoFocus isOpen width={375}>
       <label>
           Angle:
-          <input type="text" style={{marginLeft: "0.25em"}} value={degreeAngle} onChange={handleDegreeAngleChange} className="small-input" />
+          <input type="text" style={{marginLeft: "0.25em"}} value={props.degreeAngle} onChange={props.handleDegreeAngleChange} className="small-input" />
           ° ≈
-          <input type="text" style={{marginLeft: "0.8em", marginRight: "0.25em"}} value={radianAngle} onChange={handleRadianAngleChange} className="medium-input" />
+          <input type="text" style={{marginLeft: "0.8em", marginRight: "0.25em"}} value={props.radianAngle} onChange={props.handleRadianAngleChange} className="medium-input" />
           rad
       </label>
     </Menu>
