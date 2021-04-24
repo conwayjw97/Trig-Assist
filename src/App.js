92d7432fd97a4b6dbeed6f70ba203fda20ea1a1c
James <conwayjw97@gmail.com>
2021-04-24 15:45:16 +0100

    Settings menu update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 function App() {
   const [degreeAngle, setDegreeAngle] = useState(null);
   const [radianAngle, setRadianAngle] = useState(null);
+  const [trigValues, setTrigValues] = useState({});
   const [updateCount, setUpdateCount] = useState(0);
 
   const handleDegreeAngleChange = (event) => {
@@ -18,6 +19,14 @@ function App() {
     if(parseInt(input) >= 0 && parseInt(input) <= 360){
       setDegreeAngle(input);
       setRadianAngle(degToRad(input).toFixed(5));
+      setTrigValues({
+        "cos": Math.cos(degToRad(input)).toFixed(5),
+        "sin": Math.sin(degToRad(input)).toFixed(5),
+        "tan": Math.tan(degToRad(input)).toFixed(5),
+        "cot": Math.atan(degToRad(input)).toFixed(5),
+        "sec": 1/Math.cos(degToRad(input)).toFixed(5),
+        "csc": 1/Math.sin(degToRad(input)).toFixed(5),
+      });
     }
     else{
       console.log("ANGLE OUT OF BOUNDS");
@@ -28,6 +37,14 @@ function App() {
     const input = event.target.value;
     setRadianAngle(input);
     setDegreeAngle(radToDeg(input).toFixed(0));
+    setTrigValues({
+      "cos": Math.cos(input).toFixed(5),
+      "sin": Math.sin(input).toFixed(5),
+      "tan": Math.tan(input).toFixed(5),
+      "cot": Math.atan(input).toFixed(5),
+      "sec": 1/Math.cos(input).toFixed(5),
+      "csc": 1/Math.sin(input).toFixed(5),
+    });
   }
 
   const handleUpdateClicked = (event) => {
@@ -36,7 +53,14 @@ function App() {
 
   return (
     <div className="App">
-      <SettingsMenu degreeAngle={degreeAngle} radianAngle={radianAngle} handleDegreeAngleChange={handleDegreeAngleChange} handleRadianAngleChange={handleRadianAngleChange} handleUpdateClicked={handleUpdateClicked}/>
+      <SettingsMenu
+        degreeAngle={degreeAngle}
+        radianAngle={radianAngle}
+        handleDegreeAngleChange={handleDegreeAngleChange}
+        handleRadianAngleChange={handleRadianAngleChange}
+        handleUpdateClicked={handleUpdateClicked}
+        trigValues={trigValues}
+        />
       <Canvas radianAngle={radianAngle} updateCount={updateCount} />
     </div>
   );
--- src/components/settings_menu/SettingsMenu.js
+++ src/components/settings_menu/SettingsMenu.js
@@ -6,17 +6,36 @@ function SettingsMenu(props) {
   return (
     <Menu className="settings-menu" left noOverlay disableAutoFocus isOpen width={375}>
       <button onClick={props.handleUpdateClicked} className="wide-button">Update</button>
-      <label style={{marginTop: "1em"}}>
-          Angle:
+      <label style={{padding: "1em 0", borderBottom: "1px solid black"}}>
+          θ:
           <input type="text" style={{marginLeft: "0.25em"}} value={props.degreeAngle} onChange={props.handleDegreeAngleChange} className="small-input" />
           ° ≈
           <input type="text" style={{marginLeft: "0.8em", marginRight: "0.25em"}} value={props.radianAngle} onChange={props.handleRadianAngleChange} className="medium-input" />
           rad
       </label>
-      <label style={{marginTop: "1em"}}>
-          Cos:
-          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value="0" className="small-input" />
-          <input type="checkbox" className="" />
+      <label style={{paddingTop: "1em"}}>
+          cos:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.cos} className="medium-input" />
+          <input type="checkbox" style={{marginRight: "1em"}} checked className="" />
+          sin:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.sin} className="medium-input" />
+          <input type="checkbox" checked className="" />
+      </label>
+      <label style={{paddingTop: "1em"}}>
+          tan:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.tan} className="medium-input" />
+          <input type="checkbox" style={{marginRight: "1em"}} checked className="" />
+          cot:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.cot} className="medium-input" />
+          <input type="checkbox" checked className="" />
+      </label>
+      <label style={{paddingTop: "1em"}}>
+          sec:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.sec} className="medium-input" />
+          <input type="checkbox" style={{marginRight: "1em"}} checked className="" />
+          csc:
+          <input type="text" style={{marginLeft: "0.25em", marginRight: "0.25em"}} value={props.trigValues.csc} className="medium-input" />
+          <input type="checkbox" checked className="" />
       </label>
     </Menu>
   );
