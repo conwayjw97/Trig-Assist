25c333bf1e0a8c2d5111d28098d53ddc4d964ab9
James <conwayjw97@gmail.com>
2021-04-28 18:39:52 +0100

    Canvas load once functionality added

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -3,43 +3,49 @@ import CanvasDrawer from "./utils/CanvasDrawer.js"
 import "./Canvas.css";
 
 function Canvas(props) {
-  const canvasRef = useRef(null);
+  const canvas = useRef(null);
+  const canvasLoaded = useRef(false);
+  const canvasDrawer = useRef(null);
   const width = window.innerWidth;
   const height = window.innerHeight;
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    const canvasDrawer = new CanvasDrawer(ctx, width, height, props.angleUnit, props.trigVisible, props.circleDetails);
+    if(!canvasLoaded.current){
+      const ctx = canvas.current.getContext("2d");
+      canvasDrawer.current = new CanvasDrawer(ctx, width, height, props.angleUnit, props.trigVisible, props.circleDetails);
+      canvasLoaded.current = true;
+    }
+
     if(props.radianAngle != null){
-      canvasDrawer.drawAngleLine(props.radianAngle);
+      canvasDrawer.current.resetCanvas();
+      canvasDrawer.current.drawAngleLine(props.radianAngle);
     }
     if(props.angleSelect){
       let mouseDown = false;
-      canvas.onmousedown = (e) => {
-        const newRadians = canvasDrawer.onMouseMove(e);
+      canvas.current.onmousedown = (e) => {
+        const newRadians = canvasDrawer.current.onMouseMove(e);
         props.handleGraphRadianChange(newRadians);
         mouseDown = true;
       }
-      canvas.onmousemove = (e) => {
+      canvas.current.onmousemove = (e) => {
         if(mouseDown){
-          const newRadians = canvasDrawer.onMouseMove(e);
+          const newRadians = canvasDrawer.current.onMouseMove(e);
           props.handleGraphRadianChange(newRadians);
         }
       }
-      canvas.onmouseup = (e) => {
+      canvas.current.onmouseup = (e) => {
         mouseDown = false;
       }
     }
     else{
-      canvas.onmousedown = null;
-      canvas.onmousemove = null;
-      canvas.onmouseup = null;
+      canvas.current.onmousedown = null;
+      canvas.current.onmousemove = null;
+      canvas.current.onmouseup = null;
     }
   }, [props.updateCount]);
 
   return (
-    <canvas ref={canvasRef} width={width} height={height} className="canvas">
+    <canvas ref={canvas} width={width} height={height} className="canvas">
       <p>Your browser doesn"t support canvas.</p>
     </canvas>
   );
